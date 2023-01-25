e529d4d5f6adeb6fae68fda35d9d543952b49bfd
Sondoobo <style056811@gmail.com>
2023-01-25 23:10:31 +0900

    milestone 10 done

--- TeamF-WAS/routes/api/orders/create.js
+++ TeamF-WAS/routes/api/orders/create.js
@@ -6,14 +6,14 @@ module.exports = async function (fastify, opts) {
        
         const db = fastify.mongo.client.db('baedal')
         
-        const deliveryInfo = await db.collection('order').insert({
+        const deliveryInfo = ({
             "deliveryInfo": {
                 "status": "preparing",
                 "assignedCourier": "최배달",
                 "estimatedDeleveryTime": 15
         }})
-        const result =  await db.collection('order').insert(request.body)
-    
+        const result = await db.collection('order').insert(Object.assign(deliveryInfo, (request.body)))
+
         reply
         .code(201)
         .header('Content-type', 'application/json')
