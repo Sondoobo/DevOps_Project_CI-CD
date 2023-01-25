b95066d2f2dd1032a84a4938d603eb4152156578
Sondoobo <style056811@gmail.com>
2023-01-25 19:37:40 +0900

    modified v11 create js file

--- TeamF-WAS/routes/api/orders/create.js
+++ TeamF-WAS/routes/api/orders/create.js
@@ -12,7 +12,11 @@ module.exports = async function (fastify, opts) {
                 "assignedCourier": "최배달",
                 "estimatedDeleveryTime": 15
         }})
-        const result =  await db.collection('order').insert(deliveryInfo + request.body)
+        const result =  await db.collection('order').insert({"deliveryInfo": {
+            "status": "preparing",
+            "assignedCourier": "최배달",
+            "estimatedDeleveryTime": 15
+    }} + request.body)
     
         reply
         .code(201)
