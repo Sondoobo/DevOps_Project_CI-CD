f037462d90b946c4db09ab5deb49574e59f17cbd
Sondoobo <style056811@gmail.com>
2023-01-25 13:06:03 +0900

    modified v5 create js file

--- TeamF-WAS/routes/api/orders/create.js
+++ TeamF-WAS/routes/api/orders/create.js
@@ -5,18 +5,20 @@ module.exports = async function (fastify, opts) {
     fastify.post('/', async function (request, reply) {
        
         const db = fastify.mongo.client.db('baedal')
+        
         const deliveryInfo = await db.collection('order').insert({
             "deliveryInfo": {
                 "status": "preparing",
                 "assignedCourier": "최배달",
-                "estimatedDeleveryTime": 15
+                "estimatedDeleveryTime": 15,
+                result
         }})
         const result = await db.collection('order').insert(request.body)
     
         reply
         .code(201)
         .header('Content-type', 'application/json')
-        .send(deliveryInfo + result)
+        .send(deliveryInfo)
 
 }
 )}
