eb549545aa1da1de5c150e5f535c6b6eaa2eceb3
Sondoobo <style056811@gmail.com>
2023-01-25 12:18:19 +0900

    add deliveryInfo in create js file

--- TeamF-WAS/routes/api/orders/create.js
+++ TeamF-WAS/routes/api/orders/create.js
@@ -5,12 +5,18 @@ module.exports = async function (fastify, opts) {
     fastify.post('/', async function (request, reply) {
        
         const db = fastify.mongo.client.db('baedal')
+        const deliveryInfo = {
+            "deliveryInfo": {
+                "status": "preparing",
+                "assignedCourier": "최배달",
+                "estimatedDeleveryTime": 15
+        }}
         const result = await db.collection('order').insert(request.body)
     
         reply
         .code(201)
         .header('Content-type', 'application/json')
-        .send(result)
+        .send(deliveryInfo, result)
 
 }
 )}
