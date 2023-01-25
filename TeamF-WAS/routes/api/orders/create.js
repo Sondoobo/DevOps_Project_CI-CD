d9f85079b7321359966c7846e75c88949b7d2fd2
Sondoobo <style056811@gmail.com>
2023-01-25 12:44:08 +0900

    modified v2 deliveryInfo in create js file

--- TeamF-WAS/routes/api/orders/create.js
+++ TeamF-WAS/routes/api/orders/create.js
@@ -5,7 +5,7 @@ module.exports = async function (fastify, opts) {
     fastify.post('/', async function (request, reply) {
        
         const db = fastify.mongo.client.db('baedal')
-        const deliveryInfo = insert({
+        const deliveryInfo = await db.collection('order').insert({
             "deliveryInfo": {
                 "status": "preparing",
                 "assignedCourier": "최배달",
